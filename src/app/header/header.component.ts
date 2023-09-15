94cb176cd89605b8889c8a33637841b5337b615d
dazayy00 <134337268+dazayy00@users.noreply.github.com>
2023-09-15 04:11:18 -0600

    Work (#8)
    * add header
    
    * add header #1
    
    * add work #4
    
    * add work #4
    
    * add work #4

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HeaderService } from '../services/header.service';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+
+  
+  name: string = "Johan";
+  title: string = "Estudiante";
+  objetive: string = "aprender aa programar";
+  photo: string = "";
+  email: string = "joohan@gmail.com";
+  phone: string = "272 789 1234";
+  location: string = "Orizaba, Veracruz, México";
+  social: string = "@Dazayy00";
+
+  constructor(private headerService: HeaderService) { }
+
+  ngOnInit(): void {
+    this.headerService.getHeader().subscribe((data: any) => {
+      console.log(data);
+
+      this.name = data.name;
+      this.title = data.title;
+      this.objetive = data.objective;
+      this.photo = data.photo;
+      this.email = data.email;
+      this.phone = data.phone;
+      this.location = data.location;
+      this.social = data.social;
+    }
+    );
+  }
+
+
 
 }
